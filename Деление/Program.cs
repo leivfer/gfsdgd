edbb00a60e2fd0dfce7e4daa4357a716b1a254e1
Stephan <besogonovstepa2006@yandex.ru>
2024-09-13 15:52:11 +0400

    Add files via upload

---
+++ Деление/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+while (true)
+{
+    Console.WriteLine("Введи число!");
+    double a = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введи число!");
+    double b = double.Parse(Console.ReadLine());
+    Console.WriteLine(a / b);
+}
+
